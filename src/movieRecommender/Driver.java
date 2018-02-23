4d383168e8c4cc665e3dcf1a8f27851d717396b9
muranxiao <31270586+muranxiao@users.noreply.github.com>
2018-02-22 19:54:38 -0800

    version 1.0 delete the comment part in driver

--- src/movieRecommender/Driver.java
+++ src/movieRecommender/Driver.java
@@ -33,44 +33,6 @@ public class Driver {
             System.out.println(result[i]);
         }
 
-//        MovieRatingsList ml = new MovieRatingsList();
-//        ml.insertByRating(0,5.0);
-//        ml.insertByRating(1,5);
-//        ml.insertByRating(2,5);
-//        ml.insertByRating(3,1);
-//        ml.insertByRating(4,1);
-//        ml.insertByRating(5,1);
-//        ml.insertByRating(6,1);
-////        ml.setRating(2,3.5);
-////        System.out.println(ml.getMiddleNode());
-////        System.out.println(ml.getMedianRating());
-//
-//        ml.print();
-//        System.out.print("\n*****************\n");
-////        ml.setRating(4,2.5);
-////        ml.print();
-////        System.out.print("\n");
-//////        System.out.println(ml.getRating(2));
-//////        System.out.println(ml.getRating(5));
-//        MovieRatingNode head = ml.getHead();
-//        MovieRatingsList ml2 = ml.reverse(head);
-//        ml2.print();
-//        System.out.print("\n*****************\n");
-//        double d = ml.computeSimilarity(ml2);
-//        System.out.print(d);
-
-
-
-//        MovieRecommender recommender = new MovieRecommender();
-//
-//        // movies.csv and ratings.csv should be in the project folder
-//        recommender.loadData("movies.csv","ratings.csv");
-//        System.out.println("Loaded movie data...");
-//
-//        recommender.findRecommendations(3, 15, "recommendations");
-//        System.out.println();
-//        recommender.findAntiRecommendations(3, 15, "antiRecommendations");
-
     }
 
 
